1b8231912066c77b38cd46258d29ddf635da9be7
AlexandreSato <sato_alexandre@hotmail.com>
2021-11-25 14:35:50 -0300

    Trying dump postgresql

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
+import os
+
 from flask import Flask, render_template, url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+# src: https://github.com/Azure-Samples/flask-postgresql-app
+database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.environ['DBUSER'],
+    dbpass=os.environ['DBPASS'],
+    dbhost=os.environ['DBHOST'],
+    dbname=os.environ['DBNAME']
+)
+
 app = Flask(__name__)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=database_uri,
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+# initialize the database connection
+db = SQLAlchemy(app)
+
+# initialize database migration management
+migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    from models import Contratos
+    contratos = Contratos.query.all()
+    return render_template("index.html", contratos=contratos)
