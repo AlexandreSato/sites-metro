03a29216b5d8b1a2b1e7c732413d7219720d3a17
AlexandreSato <sato_alexandre@hotmail.com>
2021-11-26 15:53:37 -0300

    finally the query on Azure Postgresql worked

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+# Alexandre Nobuharu Sato em 26 de novembro de 2021, Ribeirão Pires - SP
+# exemplo obtido com Thais Akemi (thais.itocazu@fatec.sp.gov.br) em: https://www.youtube.com/watch?v=fkHkE4gZZVE
+
 import os
 
-from flask import Flask, render_template, url_for
-from flask_migrate import Migrate
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
 # src: https://github.com/Azure-Samples/flask-postgresql-app
-database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+database_uri = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=os.environ['DBUSER'],
     dbpass=os.environ['DBPASS'],
     dbhost=os.environ['DBHOST'],
@@ -14,6 +18,7 @@ database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.forma
 )
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 app.config.update(
     SQLALCHEMY_DATABASE_URI=database_uri,
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
@@ -21,12 +26,38 @@ app.config.update(
 
 # initialize the database connection
 db = SQLAlchemy(app)
+data = []
 
-# initialize database migration management
-migrate = Migrate(app, db)
+Base = declarative_base()
+
+class Data(Base):
+    __tablename__ = 'contratos'
+    id = Column(Integer(), primary_key=True)
+    contrato = Column(Integer())
+    fornecedor = Column(String())
+    objeto = Column(String())
+    valor = Column(Integer())
+    mes = Column(Integer())
+
+    @property
+    def serialize(self):
+        return {
+            "id":self.id,
+            "contrato":self.contrato,
+            "fornecedor":self.fornecedor,
+            "objeto":self.objeto,
+            "valor":self.valor,
+            "mes":self.mes
+        }
 
 @app.route("/")
 def index():
     from models import Contratos
     contratos = Contratos.query.all()
-    return render_template("index.html", contratos=contratos)
+    return render_template("index.html")
+
+@app.route("/api")
+def api():
+    todos_contratos = db.session.query(Data)
+    print(todos_contratos)
+    return jsonify(json_list = [i.serialize for i in todos_contratos.all()])
