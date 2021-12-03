d03ef449e0705c169e12f2d79eee1a31d7c92c23
AlexandreSato <sato_alexandre@hotmail.com>
2021-12-03 13:36:29 -0300

    add flashMsgs and begin acces control

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Alexandre Nobuharu Sato em 30 de novembro de 2021, Ribeirão Pires - SP
 import os
 
-from flask import Flask, jsonify, render_template, request, redirect, session
+from flask import Flask, jsonify, render_template, request, redirect, session, flash
 from flask_session import Session
 from werkzeug.utils import redirect
 from flask_migrate import Migrate
@@ -20,6 +20,7 @@ app.config.update(
     SQLALCHEMY_DATABASE_URI=database_uri,
     SQLALCHEMY_TRACK_MODIFICATIONS = False,
     TEMPLATES_AUTO_RELOAD = True,
+    SECRET_KEY = os.environ["SECRET_KEY"],
 
     # Configure session to use filesystem (instead of signed cookies)
     SESSION_FILE_DIR = mkdtemp(),
@@ -44,16 +45,22 @@ migrate = Migrate(app, db)
 from models import Contratos, Usuarios # common for db interactions
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    usuarios = Usuarios.query.all()
-    row = usuarios[0]
-    print(row)
-    return render_template("usuarios.html", usuarios=usuarios)
-
+    if request.method == "POST":
+        # TODO
+        if not (request.form.get("nome") and request.form.get("email") and request.form.get("senha")):
+            flash ("Miss someting")
+            return render_template("login.html")
+        usuario = request.form.get("nome")
+        usuario = Usuarios.query.filter_by(nome=usuario)
+        return render_template("usuarios.html", usuarios=usuario)
+    else:
+        return render_template("login.html")
 
 @app.route("/")
 def index():
+    flash("Site em construção")
     return render_template("index.html")
 
 @app.route("/api")
